e1d1e4bb1ca25edaf405c30fa6bca17e81025cbf
Anna Jimenez <annajimenez146@gmail.com>
2019-02-22 18:22:11 -0700

    change web.xml

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,5 +1,11 @@
 package com.skilldistillery.film.controllers;
 
+import org.springframework.stereotype.Controller;
+
+import com.skilldistillery.film.database.DatabaseAccessor;
+
+@Controller
 public class FilmController {
+//	private DatabaseAccessor d
 
 }
