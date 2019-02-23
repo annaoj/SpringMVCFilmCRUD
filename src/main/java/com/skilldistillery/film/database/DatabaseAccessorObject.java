45ef99b07179aef172ca607489fc8f8acab93fe2
Anna Jimenez <annajimenez146@gmail.com>
2019-02-22 22:01:50 -0700

    added component annotation to db

--- src/main/java/com/skilldistillery/film/database/DatabaseAccessorObject.java
+++ src/main/java/com/skilldistillery/film/database/DatabaseAccessorObject.java
@@ -9,11 +9,13 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.skilldistillery.film.entities.Actor;
 import com.skilldistillery.film.entities.Category;
 import com.skilldistillery.film.entities.Film;
 import com.skilldistillery.film.entities.Inventory;
-
+@Component
 public class DatabaseAccessorObject implements DatabaseAccessor {
 	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false";
 	private String user = "student";
