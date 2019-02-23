3ef87854b4b159a0a003112d8c664f49888df6ad
Jesse <Jesse.mena4@gmail.com>
2019-02-23 13:45:13 -0700

    Update: changes to the DatabaseAccessorObject.java to handle New Film

--- src/main/java/com/skilldistillery/film/database/DatabaseAccessorObject.java
+++ src/main/java/com/skilldistillery/film/database/DatabaseAccessorObject.java
@@ -7,7 +7,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
@@ -15,8 +17,10 @@ import com.skilldistillery.film.entities.Actor;
 import com.skilldistillery.film.entities.Category;
 import com.skilldistillery.film.entities.Film;
 import com.skilldistillery.film.entities.Inventory;
+
 @Component
 public class DatabaseAccessorObject implements DatabaseAccessor {
+
 	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false";
 	private String user = "student";
 	private String pass = "student";
@@ -27,8 +31,6 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 			System.out.println("Exception" + e.getMessage());
 		}
 	}
-	
-
 
 	@Override
 	public Film findFilmById(int filmId) throws SQLException {
@@ -92,7 +94,7 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 	}
 
 //	Select a.first_name, a.last_name,film.title from actor a Join film_actor ON film_actor.actor_id=a.id JOin film ON film_actor.film_id=film.id where film.id=2;
-	
+
 	@Override
 	public List<Actor> findActorsByFilmId(int filmId) {
 		List<Actor> la = new ArrayList<Actor>();
@@ -121,8 +123,7 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 		return la;
 
 	}
-	
-	
+
 	@Override
 	public List<Category> findCategorybyFilmId(int filmId) {
 		List<Category> categories = new ArrayList<Category>();
@@ -138,7 +139,7 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 			ResultSet rs = ps.executeQuery();
 
 			while (rs.next()) {
-				categories.add(new Category( rs.getString("category.name")));
+				categories.add(new Category(rs.getString("category.name")));
 			}
 
 			rs.close();
@@ -151,8 +152,9 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 		return categories;
 
 	}
-	
-	//Select title, description, l.name  from film JOIN language l ON film.language_id=l.id WHERE title LIKE "%test%" OR description LIKE "%test%";
+
+	// Select title, description, l.name from film JOIN language l ON
+	// film.language_id=l.id WHERE title LIKE "%test%" OR description LIKE "%test%";
 	@Override
 	public List<Film> findfilmByKeyword(String keyword) throws SQLException {
 		List<Film> filmList = new ArrayList<Film>();
@@ -174,7 +176,8 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 				filmList.add(new Film(rs.getInt("id"), rs.getString("title"), rs.getString("description"),
 						rs.getString("release_year"), rs.getInt("language_id"), rs.getInt("rental_duration"),
 						rs.getDouble("rental_rate"), rs.getInt("length"), rs.getDouble("replacement_cost"),
-						rs.getString("rating"), rs.getString("special_features"), findActorsByFilmId(rs.getInt("id")),rs.getString("language.name"), findCategorybyFilmId(rs.getInt("id"))));
+						rs.getString("rating"), rs.getString("special_features"), findActorsByFilmId(rs.getInt("id")),
+						rs.getString("language.name"), findCategorybyFilmId(rs.getInt("id"))));
 			}
 
 			rs.close();
@@ -198,7 +201,7 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 			PreparedStatement ps = conn.prepareStatement(sql);
 
 			// set parameter
-			ps.setInt(1,filmId);
+			ps.setInt(1, filmId);
 
 			ResultSet rs = ps.executeQuery();
 
@@ -218,45 +221,43 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 
 	@Override
 	public Film createFilm(Film film) throws SQLException {
-		  Film filmObj = null;
-		  Connection conn = null;
-		  try {
-		    conn = DriverManager.getConnection(URL, user, pass);
-		    conn.setAutoCommit(false); // START TRANSACTION
-		    String sql = "INSERT INTO film (title, language_id) "
-		                     + " VALUES (?,?)";
-		  
-		    PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-		    stmt.setString(1, film.getTitle());
-		    stmt.setInt(2, film.getLanguage_id());
-		    System.out.println(sql);
-		    int updateCount = stmt.executeUpdate();
-		    if (updateCount == 1) {
-		      ResultSet keys = stmt.getGeneratedKeys();
-		      if (keys.next()) {
-		        int newFilmId = keys.getInt(1);
-		        film.setId(newFilmId);
+		Film filmObj = null;
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(URL, user, pass);
+			conn.setAutoCommit(false); // START TRANSACTION
+			String sql = "INSERT INTO film (title, language_id) " + " VALUES (?,?)";
 
-		        System.out.println(newFilmId + "newfilmid");
-		        }
-		      }
-		    else {
-		    	filmObj = null;
-		    }
-		    conn.commit(); // COMMIT TRANSACTION
-		  } catch (SQLException sqle) {
-		    sqle.printStackTrace();
-		    if (conn != null) {
-		      try { conn.rollback(); }
-		      catch (SQLException sqle2) {
-		        System.err.println("Error trying to rollback");
-		      }
-		    }
-		    throw new RuntimeException("Error inserting actor " + film);
-		  }
+			PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			stmt.setString(1, film.getTitle());
+			stmt.setInt(2, film.getLanguage_id());
+			System.out.println(sql);
+			int updateCount = stmt.executeUpdate();
+			if (updateCount == 1) {
+				ResultSet keys = stmt.getGeneratedKeys();
+				if (keys.next()) {
+					int newFilmId = keys.getInt(1);
+					film.setId(newFilmId);
+
+					System.out.println(newFilmId + "newfilmid");
+				}
+			} else {
+				filmObj = null;
+			}
+			conn.commit(); // COMMIT TRANSACTION
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+			if (conn != null) {
+				try {
+					conn.rollback();
+				} catch (SQLException sqle2) {
+					System.err.println("Error trying to rollback");
+				}
+			}
+			throw new RuntimeException("Error inserting actor " + film);
+		}
 
 		return filmObj;
 	}
-
 
 }
